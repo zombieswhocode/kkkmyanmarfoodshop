35fa3b5c5036de42badd5cc577739c521e93120c
Nay Lin Aung <nay@global-connect.asia>
2014-09-05 23:21:24 +0630

    added controllers

---
+++ app/controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+
+class DashboardController extends \BaseController {
+
+	/**
+	 * Display a couple of widgets.
+	 *
+	 * @return Response
+	 */
+	public function home()
+	{
+		echo 'dashboard home';
+	}
+
+
+
+
+
+
+
+}
--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('/', [
+    'as' => 'Home',
+    'uses' => 'DashboardController@home'
+]);
